700eb05f730b30cb7fcb1e5150e0d20fd8d4db9f
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-07-05 17:39:13 -0600

    re-did a few of the assignments

---
+++ strings-input-output.py
@@ -0,0 +1,21 @@
+print ('hello world')
+
+name = 'Sarah'
+print(name)
+
+# store_name = 'Sarah's Store' 
+    # This variable cannot be saved, because the interpreter does not know what to do with the third ('), so we would use ""
+    # instead, so we can still distinguish that the store is Sarah's
+
+store_name = "Sarah's Store"
+print(store_name)
+
+hello = "Hello"
+name = "Sarah"
+greeting = hello + " " + name
+print(greeting)
+
+hello = "Hello"
+my_name = input("What's your name?\n")
+greeting1 = hello + " " + my_name
+print (greeting1)
---
+++ tax-calculator.py
@@ -0,0 +1,5 @@
+amount = 10
+tax = .06
+total = amount + amount*tax
+
+print(total)
