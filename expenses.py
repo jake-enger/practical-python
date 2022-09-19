343c092ecd9fad4c63f709d136fe6fa041a5cced
Jake Enger <99782876+jake-enger@users.noreply.github.com>
2022-09-19 17:21:44 -0600

    Finally getting back into it, its starting to make sense and things are clicking, even if I am behind

---
+++ expenses.py
@@ -0,0 +1,14 @@
+##expenses = [10.50, 8, 5, 15, 20, 5, 3]
+##total = sum(expenses)
+##print("You spent $", total, " on lunch this week.", sep = "")
+
+total = 0
+expenses = []
+num_expenses = int(input("Enter number of expenses:"))
+for i in range(num_expenses):
+    expenses.append(float(input("Enter an expense:")))
+
+total = sum(expenses)
+
+print("You spent $", total, sep="")
+
